422f2de277494ce523745eebcce42f2b395f4819
Andy Gillon <aawgillon@gmail.com>
2021-05-13 12:53:29 +0100

    Fixed typo

--- components/CookiePanel.vue
+++ components/CookiePanel.vue
@@ -4,9 +4,7 @@
       <p>
         We use cookies to analyse traffic and improve your experience. Learn
         more in our
-        <nuxt-link to="/legal/privacy-policy"
-          >Cookie and Privacy policy.</nuxt-link
-        >
+        <nuxt-link to="/legal/privacy-policy">Privacy policy.</nuxt-link>
       </p>
       <a class="cta-link">Continue</a>
     </div>
