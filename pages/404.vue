86c24816812224ffbcce397c5fa4c163513a8d22
Andy Gillon <aawgillon@gmail.com>
2021-06-11 12:33:47 +0100

    404

--- pages/404.vue
+++ pages/404.vue
@@ -39,7 +39,8 @@ export default {
     return redirect("/");
   }, */
   beforeMount() {
-    this.$router.push("/");
+    // this.$router.push("/");
+    this.$nuxt.$options.router.push("/");
   },
 };
 </script>
